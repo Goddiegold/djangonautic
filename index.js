0b7bdc81ad8768f75e610d7f6bdf1fdc7c4f7479
Godwin Ehikhamhen <gehikhamhen247@gmail.com>
2023-01-21 12:39:33 +0100

    added the frontend.

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const cors = require("cors");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const morgan = require("morgan")
+var history = require('connect-history-api-fallback');
+const path = require('path')
  
 
  if (!config.get("jwtPrivateKey")) {
@@ -15,18 +17,27 @@ const morgan = require("morgan")
    process.exit(1);
  }
  
- 
+ app.use(history())
+app.use(express.static("view"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "index.html"));
+}); 
+
+app.use(morgan("dev"))
+app.use(cors());
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+app.use(express.json({limit: '5mb'}));
+app.use('/api/articles',articles)
+app.use('/api/users',users)
+
  mongoose
    .connect(config.get("db"))
    .then(() => console.log("Connected to MongoDB, [/^_^/]..."))
   .catch(err => console.error("Couldn't connect to MongoDB", err));
 
-  app.use(morgan("dev"))
-  app.use(cors());
- app.use(express.urlencoded({ extended: true, limit: "5mb" }));
- app.use(express.json({limit: '5mb'}));
-app.use('/api/articles',articles)
-app.use('/api/users',users)
+  
+ 
+
 // app.use('/api/auth', auth)
 
 

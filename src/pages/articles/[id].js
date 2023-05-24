003905ff0070e656f858524e7dc82f979f5330bd
Godwin Ehikhamhen <gehikhamhen247@gmail.com>
2023-05-24 16:03:23 +0100

    done.

--- src/pages/articles/[id].js
+++ src/pages/articles/[id].js
@@ -3,6 +3,8 @@ import { getAllArticles } from '@/services/userService';
 import { getArticle } from '@/services/userService';
 import styles from "@/styles/App.module.css"
 import Image from 'next/image';
+import useSWR from 'swr'
+import { useRouter } from 'next/router';
 
 
 export async function getStaticProps({ params }) {
@@ -20,19 +22,22 @@ export async function getStaticPaths() {
     const response = await getAllArticles()
     response.data.map(article => paths.push({ params: { id: `${article._id}` } }))
     console.log("paths", paths)
-    return { paths, fallback: "blocking" }
+    return { paths, fallback: false }
 }
 function ArticleDetail({ article }) {
+    const { query } = useRouter()
+    console.log(query);
+
     return (
-            <div className={styles["article-detail"]}>
-                <div className={styles["article"]}>
-                    <Image src={article.thumb ? article.thumb : "/img/default.png"} alt='' width={400} height={400} />
-                    <h2>{article.title}</h2>
-                    <p>{article.body}</p>
-                    <p>{article.date.substring(0, 10)} at {article.date.substring(11, 16)}</p>
-                    <p className={styles.author}>added by {article.author}</p>
-                </div>
+        <div className={styles["article-detail"]}>
+            <div className={styles["article"]}>
+                <Image src={article.thumb ? article.thumb : "/img/default.png"} alt='' width={400} height={400} />
+                <h2>{article.title}</h2>
+                <p>{article.body}</p>
+                <p>{article.date.substring(0, 10)} at {article.date.substring(11, 16)}</p>
+                <p className={styles.author}>added by {article.author}</p>
             </div>
+        </div>
 
     );
 }
